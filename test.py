afdd11c754dc7d0e620005bf1a35b9be8004b272
SiDoroFF <sid.sidoroff@gmail.com>
2018-09-06 19:01:14 +0800

    fix Procfile

--- test.py
+++ test.py
@@ -26,7 +26,7 @@ class BotHandler:
         if len(get_result) > 0:
             last_update = get_result[-1]
         else:
-            last_update = get_result[len(get_result)-1]
+            last_update = get_result[len(get_result)]
 
         return last_update
 
